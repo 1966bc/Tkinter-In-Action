1fa8621bb2a679db51e7971ba20ab0f55d5d4d21
1966bc <giuseppecostanzi@gmail.com>
2019-09-20 16:48:10 +0200

    Add files via upload

---
+++ Chapter-07/checkbox.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+import tkinter as tk
+
+class ComboBoxFrame(tk.Frame):
+    def __init__(self, ):
+        super().__init__()
+
+        panel = tk.Frame()
+        tk.Checkbutton(panel,text ='Alpha',).place(x=35, y=40)
+        tk.Checkbutton(panel, text ='Beta',).place(x=35, y=60)
+        tk.Checkbutton(panel, text ='Gamma',).place(x=35, y=80) 
+        panel.pack(fill=tk.BOTH, expand=1)
+                 
+def main():
+    app = tk.Tk()
+    app.geometry("150x200")
+    app.title('Checkbox Example')
+    frame = ComboBoxFrame()
+    app.mainloop()
+    
+if __name__ == '__main__':
+    main()
