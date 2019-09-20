b6fce138e64685e92a45a002d8385790f26e7a0a
1966bc <giuseppecostanzi@gmail.com>
2019-09-20 16:39:47 +0200

    Add files via upload

---
+++ Chapter-07/button.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+import tkinter as tk
+from tkinter import ttk
+
+class ComboBoxFrame(tk.Frame):
+    def __init__(self, ):
+        super().__init__()
+
+        panel = tk.Frame()
+        self.button = ttk.Button(panel, text="Hello", command=self.OnClick)
+        self.button.place(x=50, y=20)
+        panel.pack(fill=tk.BOTH, expand=1)
+
+    def OnClick(self, event=None):
+        self.button["text"] ="Clicked"
+                   
+def main():
+    app = tk.Tk()
+    app.geometry("300x100")
+    app.title('Button Example')
+    frame = ComboBoxFrame()
+    app.mainloop()
+    
+if __name__ == '__main__':
+    main()
